3aa5f413f8dab6ade0ea8ba2a21bab7aae2acd49
kylestech95 <kylestech95@outlook.com>
2024-03-09 12:24:12 -0500

    fork: nav animation

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 const navigation = document.getElementById('navigation-bar-container');
 const nav_items = document.querySelectorAll('.list-item')
 const dropDown = () => {
-    if(navigation.classList.contains('hidden')){
+    if(navigation.classList.contains('base')){
         navigation.classList.add('appear')
-        navigation.classList.remove('hidden')
+        navigation.classList.remove('base')
     }
     // nav items drop simultaneously
     nav_items.forEach((n,index)=>{
     setTimeout(()=>{
         n.classList.add('appear')
-        n.classList.remove('hidden')
+        n.classList.remove('base')
         },75*(index+1))
     })
 
@@ -17,25 +17,17 @@ const dropDown = () => {
 const hideMe = () => {
     if(navigation.classList.contains('appear')){
         navigation.classList.remove('appear')
-        navigation.classList.add('hidden')
+        navigation.classList.add('base')
     }
     // nav items drop simultaneously
     for(let x = nav_items.length-1; x>=0; x--){
         nav_items[x].classList.remove('appear')
-        nav_items[x].classList.add('hidden')
+        nav_items[x].classList.add('base')
         console.log(nav_items[x])
     }
 }
 
-// setTimeout(()=>{
-// // dropDown()
-// // // nav items drop simultaneously
-// // nav_items.forEach((n,index)=>{
-// // setTimeout(()=>{
-// //     n.classList.toggle('appear')
-// // },75*(index+1))
-// // })
-// },1000)
+
 
 window.addEventListener('scroll',e=>{
     // get scroll position for Y
